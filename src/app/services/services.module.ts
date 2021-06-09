5a2a3aeb2752501b9f9033d78c05dfea38403a19
kaevondong <dong.696@osu.edu>
2021-06-09 11:52:38 -0400

    highlights and services

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesRoutingModule } from './services-routing.module';
+import { HighlightDirective } from './services-home/highlight.directive';
 
 
 @NgModule({
   declarations: [
+    HighlightDirective
   ],
   imports: [
     CommonModule,
